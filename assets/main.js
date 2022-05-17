b572b3a87e5f564f3b83dc071df6cb47da5ca322
adib-santos <adib.santos@code.berlin>
2022-05-17 10:38:24 +0200

    viewfinder trail

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-
+// https://medium.com/@fabiojcortes/manipulate-your-3d-content-with-gestures-in-ar-js-78da4c076607
 // Number of fingers determines action
 var sceneEL = document.getElementById('scene'); 
 
