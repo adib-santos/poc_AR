10906d429423b9f062257b2805b33bb6d0e70faa
adib-santos <adib.santos@code.berlin>
2022-05-17 12:42:58 +0200

    ..

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,44 @@
 
 
 
+var sceneEL = document.getElementById('scene'); 
+
+sceneEL.addEventListener('onefingermove', handleRotation); 
+
+sceneEL.addEventListener('twofingermove', handleScale); 
+
+
+// Makes scaling and moving only available when the camera is directed at the marker
+sceneEL.addEventListener('markerFound', (e) => {
+    isMarkerVisible = true; 
+})
+
+sceneEL.addEventListener('markerLost', (e) => {
+    isMarkerVisible = false; 
+})
+
+handleRotation(event) {
+    if (isMarkerVisible) {
+      el.object3D.rotation.y +=
+        event.detail.positionChange.x * rotationFactor;
+
+      el.object3D.rotation.x +=
+        event.detail.positionChange.y * rotationFactor;
+    }
+  }
+
+  handleScale(event) {
+    if (isMarkerVisible) {
+      this.scaleFactor *=
+        1 + event.detail.spreadChange / event.detail.startSpread;
+
+      this.scaleFactor = Math.min(
+        Math.max(this.scaleFactor, this.data.minScale),
+        this.data.maxScale
+      );
+
+      el.object3D.scale.x = scaleFactor * initialScale.x;
+      el.object3D.scale.y = scaleFactor * initialScale.y;
+      el.object3D.scale.z = scaleFactor * initialScale.z;
+    }
+  }
