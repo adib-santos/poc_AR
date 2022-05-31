2bf4a94bb822a564e26581e872451f9dfac53672
Eva Lam <eva.lam@code.berlin>
2022-05-31 14:59:24 +0200

    added new qr code

--- main.js
+++ main.js
@@ -1,48 +1,150 @@
-// https://medium.com/@fabiojcortes/manipulate-your-3d-content-with-gestures-in-ar-js-78da4c076607
-// Number of fingers determines action
+let codes = [];
+const seen = new Set();
+// Create new barcode detector
+const barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] });
 
+// Define custom element
+customElements.define('scaned-item', class extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.querySelector('#scaned-item').content;
+    const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(template.cloneNode(true));
+  };
+});
 
-/*
-var sceneEL = document.getElementById('scene'); 
+// Codes proxy/state
+const codesProxy = new Proxy(codes, {
+  set (target, prop, value, receiver) {
+    // Throw err if value is a number
+    // Stops from saving undefined codes
+    if (typeof value === 'number') throw value;
+    
+    target.push(value);
 
-sceneEL.addEventListener('onefingermove', handleRotation); 
+    // Check if code has already been scanned
+    target = target.filter((c) => {
+      if (c.rawValue !== window.barcodeVal) return c;
+      const d = seen.has(c.rawValue);
+      seen.add(c.rawValue);
+      return !d;
+    })
+    
+    // Select the container scanned
+    const scanned = document.querySelector('#scanned')
+    const temp = document.createElement('scaned-item')
+    const format = document.createElement('span')
+    const rawValue = document.createElement('span')
 
-sceneEL.addEventListener('twofingermove', handleScale); 
+    // Goes into the custom elements formate slot
+    format.setAttribute('slot', 'format');
+    format.innerHTML = value.format;
+    
+    // Goes into the custom elements raw slot 
+    rawValue.setAttribute('slot', 'raw');
+    rawValue.innerHTML = value.rawValue;
 
+    // Append elements to custom element
+    temp.appendChild(rawValue);
+    temp.appendChild(format);
 
-// Makes scaling and moving only available when the camera is directed at the marker
-sceneEL.addEventListener('markerFound', (e) => {
-    isMarkerVisible = true; 
-})
+    // Append Custom element to scanned container
+    scanned.appendChild(temp);
+    return true;
+  }
+});
 
-sceneEL.addEventListener('markerLost', (e) => {
-    isMarkerVisible = false; 
-})
+// Get video element 
+const video = document.getElementById('video');
 
-handleRotation(event) {
-    if (isMarkerVisible) {
-      el.object3D.rotation.y +=
-        event.detail.positionChange.x * rotationFactor;
+// Check for a camera
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  const constraints = {
+    video: true,
+    audio: false
+  };
+  
+  // Start video stream
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => video.srcObject = stream);
+}
 
-      el.object3D.rotation.x +=
-        event.detail.positionChange.y * rotationFactor;
+// Draw outline to canvas 
+/* --NOTE-- 
+  Some codes will not out line the whole barcode 
+  instead may have a thin line this is because for a lot of 
+  barcodes that is all that is needed.
+  if you would like to out line the whole code you can have 
+  a look at using the boundingBox object instead of 
+  the cornerPoints array in this example it is not used 
+  but my edit this to make a square around other codes
+  that do not get outlined :) 
+*/
+const drawCodePath = ({cornerPoints}) => {
+  const canvas = document.querySelector('#canvas');
+  const ctx = canvas.getContext('2d');
+  const strokeGradient = ctx.createLinearGradient(0, 0, canvas.scrollWidth, canvas.scrollHeight);
+  
+  // Exit function and clear canvas if no corner points
+  if (!cornerPoints) return ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Clear canvas for new redraw
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Create new gradient
+  strokeGradient.addColorStop('0', '#c471ed');
+  strokeGradient.addColorStop('1', '#f7797d');
+
+  // Define stoke styles
+  ctx.strokeStyle = strokeGradient;
+  ctx.lineWidth = 4;
+
+  // Begin draw
+  ctx.beginPath();
+
+  // Draw code outline path
+  for (const [i, {x, y}] of cornerPoints.entries()) {
+    if (i === 0) {
+      // Move x half of the stroke width back 
+      // makes the start and end corner line up
+      ctx.moveTo(x - ctx.lineWidth/2, y);
+      continue;
     }
+
+    // Draw line from current pos to x, y
+    ctx.lineTo(x, y);
+
+    // Complete square draw to starting position
+    if (i === cornerPoints.length-1) ctx.lineTo(cornerPoints[0].x, cornerPoints[0].y);
   }
 
-  handleScale(event) {
-    if (isMarkerVisible) {
-      this.scaleFactor *=
-        1 + event.detail.spreadChange / event.detail.startSpread;
+  // Make path to stroke
+  ctx.stroke();
+}
 
-      this.scaleFactor = Math.min(
-        Math.max(this.scaleFactor, this.data.minScale),
-        this.data.maxScale
-      );
+// Detect code function 
+const detectCode = () => {
+  barcodeDetector.detect(video).then(codes => {
+    // If no codes exit function and clear canvas
+    if (codes.length === 0) return drawCodePath({});
+    
+    for (const barcode of codes)  {
+      console.log(barcode)
+      // Draw outline
+      drawCodePath(barcode);
+      
+      // Code in seen set then exit loop 
+      if (seen.has(barcode.rawValue)) return;
 
-      el.object3D.scale.x = scaleFactor * initialScale.x;
-      el.object3D.scale.y = scaleFactor * initialScale.y;
-      el.object3D.scale.z = scaleFactor * initialScale.z;
+      // Save barcode to window to use later on
+      // then push to the codes proxy
+      window.barcodeVal = barcode.rawValue;
+      codesProxy.push(barcode);
+
     }
-  }
+  }).catch(err => {
+    console.error(err);
+  })
+}
 
-  */
+// Run detect code function every 100 milliseconds
+setInterval(detectCode, 100);
+
